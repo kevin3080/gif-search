0788cf79915c11d59a2f3c2ead1a64583e09ce24
Kevin Pernia <kevinpernia29@gmail.com>
2023-11-27 18:55:09 -0500

    ajustes

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,9 @@ export const getGifs = async(categorys) => {
 
     // Modificar el link con tu propia api key
     // https://api.giphy.com/v1/gifs/search?api_key=${YOUR_APY_KEY}&q=${categorys}&limit=10
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_API_KEY}&q=${categorys}&limit=10`;
+
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${categorys}&limit=10`;
     const resp = await fetch(url); // peticion http
     const { data = [] } = await resp.json();
 
